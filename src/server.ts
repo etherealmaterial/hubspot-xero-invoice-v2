8b10740c9ad4fed060a6b55dd57ee0221935a977
Scott Brown <62860464+etherealmaterial@users.noreply.github.com>
2024-08-25 17:25:12 +1000

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
